af33ad5d72008461da0bfe7bec7189d8254e8b30
dato-svg <139239441+dato-svg@users.noreply.github.com>
2023-12-06 19:48:05 +0400

    asa
    sa

---
+++ thiefGame/Assets/LogoScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class LogoScene : MonoBehaviour
+{
+    private VideoPlayer player;
+    public string url;
+
+    private void Awake()
+    {
+        player = GetComponent<VideoPlayer>();
+        player.url = url;
+    }
+
+    private void Start()
+    {
+        
+        StartCoroutine(NewSceneLogo());
+    }
+
+
+    private IEnumerator NewSceneLogo()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(1);
+    }
+}
